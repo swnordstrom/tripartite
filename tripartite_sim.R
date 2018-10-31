5462628c2adcf157bc8b74d331496354f28c728b
swnordstrom <swnordstrom@email.wm.edu>
2018-10-31 14:04:37 -0600

    finish sim stable groups()

--- tripartite_sim.R
+++ tripartite_sim.R
@@ -41,25 +41,38 @@ sim.fission.fusion = function(indvs.total, sites.total, times.total, prob.move) 
   
 }
 
-# sim.stable.groups = 
-
-# indvs.total = 5
-# sites.total = 5
-# times.total = 10
-# prob.move = 0.5
-# prob.follow = 0.9
-# groups = c(rep(1,3), rep(2,3), rep(3,3))
-
-a[,,1] = t(replicate(indvs.total, sample(c(rep(0,sites.total-1), 1))))
-
-for (t in 2:times.total) { 
-  for (g in groups) {
-    group.target = sample(1:sites.total, 1)
-    for (k in 1:which(groups %in% g)) {
-      if (runif(1) < prob.move) { 
-        if (runif(1) < prob.follow) a[k,,t] = group.target
-        } else a[k,,t] = sample((1:sites.total)[-group.target], 1)
-      } else a[k,,t] = a[k,,t-1]
+sim.stable.groups = function(indvs.total, sites.total, times.total, prob.follow, groups) {
+  
+  # indvs.total = 9
+  # sites.total = 5
+  # times.total = 10
+  # prob.move = 0.5
+  # prob.follow = 0.9
+  # groups = c(rep(1,3), rep(2,3), rep(3,3))
+  
+  a = array(dim = c(indvs.total, sites.total, times.total))
+  
+  inits = t(replicate(length(unique(groups)), sample(c(rep(0,sites.total-1), 1))))
+  for (g in groups) for (k in which(groups %in% g)) a[k,,1] = inits[g,]
+  
+  for (t in 2:times.total) { 
+    for (g in groups) {
+      group.target = sample(1:sites.total, 1)
+      for (k in which(groups %in% g)) {
+        if (runif(1) < prob.follow) { 
+          a[k,,t] = 0 
+          a[k,group.target,t] = 1
+        } else { 
+          a[k,,t] = 0
+          a[k,sample((1:sites.total)[-group.target], 1),t] = 1
+        }
+      }
     }
   }
+  
+  return(a)
+  
 }
+
+####
+
